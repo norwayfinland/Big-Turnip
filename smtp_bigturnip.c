c3f472b2086d0e4e31d80f9b5b0475643790314a
Nathan "Big Turnip" Fowler <lists@packetmail.net>
2022-08-16 20:38:32 -0500

    Add files via upload
    Like uh, freaking defragged the registry and then umm... you know... like ... um optimized the battery charging and replaced the battery cables.  This made the C code like totally twice as fast.  Oh also removed unused icons from the desktop and increased quality on all actionable items.

--- smtp_bigturnip.c
+++ smtp_bigturnip.c
@@ -11,10 +11,14 @@
  * Changelog
  *      2022-08-16 - Use 0x09 where possible to piss off the people who prefer 0x20 four times, literally, four bytes of bloat
  *	instead of using a single byte 0x09 where it makes sense.  Oh yeah, also provide some level of visceral
- *	feedback that we just hauled off and kicked a miscreant in the nards using TCP and /dev/random.
+ *	feedback that we just hauled off and kicked a miscreant in the nards using TCP and /dev/urandom.
  *
  *	Also add better directional logging because that's better than using TCPDump to watch the port.  I'm gonna feature
  *	creep the hell out of this maybe.  Who knows.  Like me and follow me on Shitter peepz so I can pump my self esteem.
+ *
+ *	Also some dbag up in LACNIC keeps doing a repeated connect and QUIT so meh, enjoy a new phase of entropy 'n stuff.
+ *
+ *	Oh yeah, and like, change strstr() to stristr() cause reasons.  Oh nope, nevermind, C doesn't do that.  Oops.
  *
 */
 
@@ -77,22 +81,6 @@ static int getLine( char *prompt, char *buff, size_t sz) {
 }
 
 static int Validate_and_Log (int rc, char *response) {
-	//Random Delay
-	FILE* urandom;
-	unsigned int seed;
-	int rnd_offset;
-
-	//Init rand()
-	srand((unsigned)time(NULL));
-	urandom = fopen("/dev/urandom", "r");
-	if (urandom != NULL) {
-		if( fread (&seed, sizeof(seed), 1, urandom) == 1 ){ srand(seed);} 
-		fclose(urandom);
-	}
-
-	//Random-wait before returning to the caller to best simulate a busy MTA
-	rnd_offset = (rand() % 5000) * 1000; usleep(rnd_offset);
-
 	//Validation and error handling
 	if ( rc != OK ){
 		if ( rc == NO_INPUT ){
@@ -127,53 +115,35 @@ static int Validate_and_Log (int rc, char *response) {
 	return 0;
 }
 
-int main(void) {
-	//String reading
-	char response[4096];
-	int rc = 0;
+static int Random_Wait(){
+	//Random Delay
+	FILE* urandom;
+	unsigned int seed;
+	int rnd_offset;
 
-	//Send the banner and get the response
-	if (Validate_and_Log(rc, "> 220 localhost ESMTP Use of this system for unsolicited electronic mail advertisements (UCE), SPAM, or malicious content is forbidden.\n") != 0) { return 1; }
-	rc  = getLine("220 localhost ESMTP Use of this system for unsolicited electronic mail advertisements (UCE), SPAM, or malicious content is forbidden.\n", response, sizeof(response));
-	if (Validate_and_Log(rc, response) != 0) { return 1; }
-
-	//Did they even attempt to HELO or EHLO?
-	if ( strstr(response, "EHLO ") == NULL && strstr(response,"HELO ") == NULL && strstr(response, "ehlo ") == NULL && strstr(response,"helo ") ){
-		if (Validate_and_Log(rc, "> 502 5.5.2 Error: command not recognized\n") != 0) { return 1; }
-		rc = getLine("502 5.5.2 Error: command not recognized\n", response, sizeof(response));
-		if (Validate_and_Log(rc, response) != 0) { return 1; }
+	//Init rand()
+	srand((unsigned)time(NULL));
+	urandom = fopen("/dev/urandom", "r");
+	if (urandom != NULL) {
+		if( fread (&seed, sizeof(seed), 1, urandom) == 1 ){ srand(seed); }
+		fclose(urandom);
 	}
 
-	//If they're still being stupid here and cannot HELO or HELO lets terminate the connection
-	if ( strstr(response, "EHLO ") == NULL && strstr(response,"HELO ") == NULL && strstr(response, "ehlo ") == NULL && strstr(response,"helo ") ){
-		if (Validate_and_Log(rc, "> 502 5.5.2 Error: command not recognized\n") != 0) { return 1; }
-		printf("502 5.5.2 Error: command not recognized\n");
-		fflush(stdout);
-		return 1;
-	}
+	//Random-wait before returning to the caller to best simulate a busy MTA
+	rnd_offset = (rand() % 5000) * 1000;
+	usleep(rnd_offset);
 
-	//Did they EHLO instead of HELO?
-	if ( strstr(response, "EHLO ") != NULL || strstr(response, "ehlo ") != NULL ){
-		if (Validate_and_Log(rc, "> 250-localhost\\n250-PIPELINING\\n250-SIZE 20480000\\n250-VRFY\\n250-ETRN\\n250-ENHANCEDSTATUSCODES\\n250-8BITMIME\\n250 DSN\\n") != 0) { return 1; }
-		rc = getLine("250-localhost\n250-PIPELINING\n250-SIZE 20480000\n250-VRFY\n250-ETRN\n250-ENHANCEDSTATUSCODES\n250-8BITMIME\n250 DSN\n", response, sizeof(response));
-		if (Validate_and_Log(rc, response) != 0) { return 1; }
-	}
+	//Allow the SMTP conversation to persist
+	return 0;
+}
 
-	//After the EHLO/HELO get the next command, potentially RCPT TO
-	if (Validate_and_Log(rc, "> 250 localhost\n") != 0) { return 1; }
-	rc  = getLine("250 localhost\n", response, sizeof(response));
-	if (Validate_and_Log(rc, response) != 0 ) { return 1; }
-
-	//Get the final command before auto-starting the entropy engine, potentially MAIL FROM
-	if (Validate_and_Log(rc, "> 250 2.1.0 OK\n") != 0) { return 1; }
-	rc  = getLine("250 2.1.0 OK\n", response, sizeof(response));
-	if (Validate_and_Log(rc, response) != 0) { return 1; }
-
+static int Entropy_Engine(){
 	//Summon the chaos entropy engine output, where we stop, no one knows until we hit EOF.  There are no big bucks though, only whammies for miscreants.
 	//Not using feof() here because well, /dev/urandom is ... random.  No idea how big the buffer is until EOF so better to read a single character, I think.
 	FILE* urandom;
 	char random_data;
 	int data_count = 0;
+	int rc = 0;
 
 	urandom = fopen("/dev/urandom", "r");
 	if (urandom != NULL) {
@@ -188,6 +158,67 @@ int main(void) {
 		fclose(urandom);
 	}
 
-	//We done
+	//Allow the SMTP conversation to persist
+	return 0;
+}
+
+int main(void) {
+	//String reading
+	char response[4096];
+	int rc = 0;
+
+	//Send the banner and get the response
+	Random_Wait();
+	if (Validate_and_Log(rc, "> 220 localhost ESMTP Use of this system for unsolicited electronic mail advertisements (UCE), SPAM, or malicious content is forbidden.\n") != 0) { return 1; }
+	rc  = getLine("220 localhost ESMTP Use of this system for unsolicited electronic mail advertisements (UCE), SPAM, or malicious content is forbidden.\n", response, sizeof(response));
+	if (Validate_and_Log(rc, response) != 0) { return 1; }
+
+	//Are they just wasting our time, enumerating and scanning for SMTP servers?  No one likes a quitter, give them some free data like a participation trophy so they feel
+	//better about themselves!
+	if ( strstr(response, "QUIT") != NULL || strstr(response,"RSET") != NULL || strstr(response, "quit") != NULL || strstr(response,"rset") != NULL ){
+		//The sun is hot, it hurts my eyes.  Script kiddies melt like snowflakes from the exhaust of the entropy engine.
+		Entropy_Engine();
+		return 0;
+	}
+
+	//Did they even attempt to HELO or EHLO?
+	if ( strstr(response, "EHLO ") == NULL && strstr(response,"HELO ") == NULL && strstr(response, "ehlo ") == NULL && strstr(response,"helo ") == NULL ){
+		Random_Wait();
+		if (Validate_and_Log(rc, "> 502 5.5.2 Error: command not recognized\n") != 0) { return 1; }
+		rc = getLine("502 5.5.2 Error: command not recognized\n", response, sizeof(response));
+		if (Validate_and_Log(rc, response) != 0) { return 1; }
+	}
+
+	//If they're still being stupid here and cannot HELO or HELO lets terminate the connection
+	if ( strstr(response, "EHLO ") == NULL && strstr(response,"HELO ") == NULL && strstr(response, "ehlo ") == NULL && strstr(response,"helo ") == NULL ){
+		Random_Wait();
+		if (Validate_and_Log(rc, "> 502 5.5.2 Error: command not recognized\n") != 0) { return 1; }
+		printf("502 5.5.2 Error: command not recognized\n");
+		fflush(stdout);
+		return 1;
+	}
+
+	//Did they EHLO instead of HELO?
+	if ( strstr(response, "EHLO ") != NULL && strstr(response, "ehlo ") != NULL ){
+		Random_Wait();
+		if (Validate_and_Log(rc, "> 250-localhost\\n250-PIPELINING\\n250-SIZE 20480000\\n250-VRFY\\n250-ETRN\\n250-ENHANCEDSTATUSCODES\\n250-8BITMIME\\n250 DSN\\n") != 0) { return 1; }
+		rc = getLine("250-localhost\n250-PIPELINING\n250-SIZE 20480000\n250-VRFY\n250-ETRN\n250-ENHANCEDSTATUSCODES\n250-8BITMIME\n250 DSN\n", response, sizeof(response));
+		if (Validate_and_Log(rc, response) != 0) { return 1; }
+	}
+
+	//After the EHLO/HELO get the next command, potentially RCPT TO
+	Random_Wait();
+	if (Validate_and_Log(rc, "> 250 localhost\n") != 0) { return 1; }
+	rc  = getLine("250 localhost\n", response, sizeof(response));
+	if (Validate_and_Log(rc, response) != 0 ) { return 1; }
+
+	//Get the final command before auto-starting the entropy engine, potentially MAIL FROM
+	Random_Wait();
+	if (Validate_and_Log(rc, "> 250 2.1.0 OK\n") != 0) { return 1; }
+	rc  = getLine("250 2.1.0 OK\n", response, sizeof(response));
+	if (Validate_and_Log(rc, response) != 0) { return 1; }
+
+	//If the connection is still here, lets assume they're jerks, and nard kick 'em with some Entropy.
+	Entropy_Engine();
 	return 0;
 }
